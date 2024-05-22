18a9c2be2e9c2aa8e3ed3659a828d9b6661964d6
ENRICO MEIRA DE PAULA <0031432412007@lti.fatecso.edu.br>
2024-05-22 17:48:52 -0300

    Iniciando as rotas

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,23 @@
+<php?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    
+    </head>
+    <body>
+
+    <header>
+        <h1>
+            @yield('header')
+        </h1>
+    </header>
+        <div class="content">
+            @yield('content')
+        </div>
+    </body>
+    </html>
+<php>
+
---
+++ resources/views/users/createUser.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base') 
+
+@section('header', 'Criando usuário')
+
+@section('content')
+    <form action="get">
+        <input type="text">
+    </form>
+@endsection
